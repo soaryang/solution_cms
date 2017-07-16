aeeb00e652174fd220b0717baa55ab4a25bb32bb
soaryang <759620299@qq.com>
2017-07-16 13:14:52 +0800

    标签禁用

--- routes/user.js
+++ routes/user.js
@@ -10,6 +10,9 @@ router.get('/userList', function(req, res, next) {
     res.render('user/userList', { title: 'Express' });
 });
 
+router.get('/roleList', function(req, res, next) {
+    res.render('user/roleList', { title: 'Express' });
+});
 router.get('/roleAdd', function(req, res, next) {
     res.render('user/roleAdd', { title: 'Express' });
 });
