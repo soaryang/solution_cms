9609e976b0b58aa40e696fc0f6080110344a50c5
soaryang <759620299@qq.com>
2017-07-29 20:59:40 +0800

    前端界面

--- public/js/index.js
+++ public/js/index.js
@@ -5,23 +5,27 @@ var init = function () {
         console.log(data);
         var html='';
         if(data!=null){
-            for(var i=0; i<data.length; i++){
-                var questionObject = data[i];
-                html+='<div style="display: inline-block; border-bottom:  1px dashed #ddd;width: 100%;">';
-                html+='<div style="float: left;margin-top: 20px;">';
-                html+='<a class="tag" style=" background-color: rgb(255,236,244); width: 50px; height: 50px;"><span>关注</span><br/><span>50</span></a>';
-                html+='<a class="tag" style=" background-color: rgb(255,236,244); width: 50px; height: 50px;"><span>回答</span><br/><span>50</span></a>';
-                html+='<a class="tag" style=" background-color: rgb(255,236,244); width: 50px; height: 50px;"><span>访问</span><br/><span>50</span></a>';
-                html+='</div>';
-                html+='<div style="float: left;margin-top: 10px;margin-left: 15px;">';
-                html+='<h4><a href="http://spring4all.com/question/328">'+questionObject.name+'</a></h4>';
-                html+='<div>';
-                html+='<a class="tag" style=" background-color: rgb(255,236,244);">'+questionObject.tagName+'</a>';
-                html+='</div>';
-                html+='</div>';
-                html+='</div>';
+            var count = data.count;
+            var array = data.questionList;
+            if(array!=null){
+                for(var i=0; i<array.length; i++){
+                    var questionObject = array[i];
+                    html+='<div style="display: inline-block; border-bottom:  1px dashed #ddd;width: 100%;">';
+                    html+='<div style="float: left;margin-top: 20px;">';
+                    html+='<a class="tag" style=" background-color: rgb(255,236,244); width: 50px; height: 50px;"><span>关注</span><br/><span>50</span></a>';
+                    html+='<a class="tag" style=" background-color: rgb(255,236,244); width: 50px; height: 50px;"><span>回答</span><br/><span>50</span></a>';
+                    html+='<a class="tag" style=" background-color: rgb(255,236,244); width: 50px; height: 50px;"><span>访问</span><br/><span>50</span></a>';
+                    html+='</div>';
+                    html+='<div style="float: left;margin-top: 10px;margin-left: 15px;">';
+                    html+='<h4><a href="javascript:void(0);">'+questionObject.name+'</a></h4>';
+                    html+='<div>';
+                    html+='<a class="tag" style=" background-color: rgb(255,236,244);">'+questionObject.tagName+'</a>';
+                    html+='</div>';
+                    html+='</div>';
+                    html+='</div>';
+                }
+                $("#questionList").html(html);
             }
-            $("#questionList").html(html);
         }else{
             $("#questionList").html('没有数据');
         }
